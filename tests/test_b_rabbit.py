d37aaf4cb1ac6c9e6d3170fffb5219e28335904f
= <nidhalbacc@gmail.com>
2020-06-24 22:46:12 +0200

    test on rabbitmq connection

--- tests/test_b_rabbit.py
+++ tests/test_b_rabbit.py
@@ -9,14 +9,20 @@ from b_rabbit import BRabbit
 import rabbitpy
 
 
+@pytest.fixture
+def rabbit():
+    return BRabbit()
+
+
 def test_connection():
     with pytest.raises(Exception):
         BRabbit(host=1234, port='')
 
 
-def test_rabbitmq_connection():
-
-    rabbit = BRabbit()
+def test_rabbitmq_connection(rabbit):
     assert rabbit
     assert isinstance(rabbit, BRabbit)
     assert isinstance(rabbit.connection, rabbitpy.Connection)
+
+# def test_publisher():
+#     rabbitpy.Connection
