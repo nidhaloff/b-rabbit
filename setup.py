64a605f7d0f07c2309ac362bb3e8ee31c8737e22
= <nidhalbacc@gmail.com>
2020-06-25 01:49:17 +0200

    stable release

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nidhaloff/b_rabbit',
-    version='1.0.0',
+    version='1.0.1',
     zip_safe=False,
 )
