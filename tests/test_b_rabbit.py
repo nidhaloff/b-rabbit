335cffc65e593c3fb006e09310a70181c7006e92
= <nidhalbacc@gmail.com>
2020-06-24 22:14:41 +0200

    added tests

--- tests/test_b_rabbit.py
+++ tests/test_b_rabbit.py
@@ -9,12 +9,9 @@ from b_rabbit import BRabbit
 
 
 def test_connection():
-    with pytest.raises(RuntimeError):
+    with pytest.raises(Exception):
         BRabbit(host=1234, port='')
 
-
-def test_something():
-    rabbit = BRabbit()
 # def test_connection(mocker):
 #     mocker.patch.object(BRabbit)
 #     BRabbit.EventPublisher.publish = 120
