5eba4778609c43270f3ba036d6450b7aad0d8c49
gabriel10619 <gabriel10619@gmail.com>
2021-07-20 20:40:05 -0400

    Update test_b_rabbit.py

--- tests/test_b_rabbit.py
+++ tests/test_b_rabbit.py
@@ -1,26 +1,18 @@
 #!/usr/bin/env python
-
 """Tests for `b_rabbit` package."""
-
 import pytest
 from b_rabbit import BRabbit
 import rabbitpy
-
-MSG = 'mock'
+import time
+MSG = 'test'
 REQUEST_MSG = 'request_msg'
 RESPONSE_MSG = 'response_msg'
-
-
 @pytest.fixture
 def rabbit():
     return BRabbit()
-
-
 def test_connection():
     with pytest.raises(Exception):
         BRabbit(host=1234, port='')
-
-
 def test_rabbitmq_connection(rabbit):
     assert rabbit
     assert isinstance(rabbit, BRabbit)
@@ -33,13 +25,14 @@ def test_publisher(rabbit):
                                       publisher_name='test',
                                       exchange_type='topic',
                                       external=False)
-    published = publisher.publish(routing_key='testing.test', payload=MSG, important=False, properties=properties.properties_dict)
+    published = publisher.publish(routing_key='testing.test', payload=MSG, important=False, properties=properties)
     assert published is True
 
 
 def test_subscriber(rabbit):
+    print("Starting test subscriber...")
     def callback(msg):
-        assert msg.body == MSG
+        assert msg.body.decode('UTF-8') == MSG
         assert msg.properties["correlation_id"] == "test_id"
 
     subscriber = rabbit.EventSubscriber(b_rabbit=rabbit,
